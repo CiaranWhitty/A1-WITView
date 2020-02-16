97fd6698a62b7c4894dae3fdf3b84759fe441431
Ciaran Whitty <whittyciaran1998@gmail.com>
2020-02-16 15:38:53 +0000

    HomeFragment - both spinners now working

--- app/src/main/java/com/example/a1_witview/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/a1_witview/ui/home/HomeFragment.kt
@@ -4,37 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.a1_witview.R
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.support.v4.toast
 
-class HomeFragment : Fragment(), AdapterView.OnItemSelectedListener{
+class HomeFragment : Fragment(), AdapterView.OnItemSelectedListener, AnkoLogger{
 
-
-    var spinner: Spinner? = null
-    var textView_msg: TextView? = null
+    lateinit var D_time: TextView
+    lateinit var D_day: TextView
     var Times = arrayOf("9.15","10.15", "11.15", "12.15","13.15","14.15", "15.15", "16.15", "17.15")
+    var Days = arrayOf("Monday","Tuesday", "Wednesday", "Thursday", "Friday")
+
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         activity?.title = getString(R.string.menu_home)
 
-        /*Find the id of spinner*/
-        val spinner = timetable_spinner
-
-        /*set an adapter with strings */
-        spinner.adapter = ArrayAdapter(
-            activity,
-            R.layout.support_simple_spinner_dropdown_item,
-            Times
-        )
 
         arguments?.let {
 
@@ -45,14 +39,53 @@ class HomeFragment : Fragment(), AdapterView.OnItemSelectedListener{
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        /*Find the id of spinner*/
+        val spinnerTimes = root.timetable_spinner_times
+        val spinnerDays = root.timetable_spinner_days
+
+        /*set an adapter with strings */
+        spinnerTimes.adapter = ArrayAdapter(activity, R.layout.support_simple_spinner_dropdown_item, Times)
+        spinnerDays.adapter = ArrayAdapter(activity, R.layout.support_simple_spinner_dropdown_item, Days)
+
+        spinnerTimes.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+                root.Display_Time.setText(Times.get(position))
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                root.Display_Time.setText("Select a time")
+
+            }
+        }
+
+        spinnerDays.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                root.Display_Day.setText(Days.get(position))
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                root.Display_Day.setText("Select a Day")
+
+            }
+        }
 
 
+        return root
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+
+
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
